226899a34be18f55f8093f58ee6ae81f59acf56e
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 15:10:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,7 +20,7 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  aoh[row[key]]
+  aoh[row][key]
 end
 # Update the AoH data at row and key to have the value of new_value
 # Return the updated AoH
